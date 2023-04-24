8d755dd56a5b66ffe424c9bca48151cf48bd11c9
David12peters <104854750+David12peters@users.noreply.github.com>
2023-04-24 09:45:25 +0100

    Add files via upload

---
+++ main/script.js
@@ -0,0 +1,457 @@
+   /*import {JSparagraph1} from './script.js'
+     
+     import {JS1} from './frame.js'
+     */
+   
+        //HTML DECLARATION                 
+     
+     let HCparagraph1 = document.getElementById("HCparagraph1")
+    let HCparagraph2 = document.getElementById("HCparagraph2")
+     let HCparagraph3 = document.getElementById("HCparagraph3")
+     let HCparagraph4 = document.getElementById("HCparagraph4")
+     let HCparagraph5 = document.getElementById("HCparagraph5")
+     let HCparagraph6 = document.getElementById("HCparagraph6")
+     let HCparagraph7 = document.getElementById("HCparagraph7")
+     let HCparagraph8 = document.getElementById("HCparagraph8")
+     let HCparagraph9 = document.getElementById("HCparagraph9")
+     let HCparagraph10 = document.getElementById("HCparagraph10")
+     let HCparagraph11 = document.getElementById("HCparagraph11")
+     let HCparagraph12 = document.getElementById("HCparagraph12")
+     let HCparagraph13 = document.getElementById("HCparagraph13")
+     let HCparagraph14 = document.getElementById("HCparagraph14")
+     let HCparagraph15 = document.getElementById("HCparagraph15")
+     
+     //JAVASCRIPT DECLARATION
+     let JSparagraph1 = document.getElementById("JSparagraph1")
+     let JSparagraph2 = document.getElementById("JSparagraph2")
+     let JSparagraph3 = document.getElementById("JSparagraph3")
+     let JSparagraph4 = document.getElementById("JSparagraph4")
+     let JSparagraph5 = document.getElementById("JSparagraph5")
+     let JSparagraph6 = document.getElementById("JSparagraph6")
+     let JSparagraph7 = document.getElementById("JSparagraph7")
+     let JSparagraph8 = document.getElementById("JSparagraph8")
+     let JSparagraph9 = document.getElementById("JSparagraph9")
+     let JSparagraph10 = document.getElementById("JSparagraph10")
+     let JSparagraph11 = document.getElementById("JSparagraph11")
+     let JSparagraph12 = document.getElementById("JSparagraph12")
+     let JSparagraph13 = document.getElementById("JSparagraph13")
+     let JSparagraph14 = document.getElementById("JSparagraph14")
+     let JSparagraph15 = document.getElementById("JSparagraph15")
+     
+     
+     //HTML IFRAMES
+     
+     const HC1 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../photo_album/OOU_photoalbum.html" width="80%" style="border: 0px;"></iframe>
+                        `
+        const HC2 = 
+                        `
+                        <p>Scroll down to view</p>
+                        <iframe name="demo" height="300" src="../Cafe_menu/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+        const HC3 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../Color_markers/index.html" width="100%" style="border: 0px;"></iframe>
+                            `
+   const HC4 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../Registration_form/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+        const HC5 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../Rothko_Painting/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+        const HC6 = 
+                      `  <iframe name="demo" height="300" src="../Photo_Gallery/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+      const HC7 = 
+                        
+                    `    <iframe name="demo" height="300" src="../Nutrition_Label/index.html" width="100%" style="border: 0px;"></iframe>
+                        `  
+        const HC8 = 
+                        
+                      `  <iframe name="demo" height="300" src="../Balancesheet/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+      
+        const HC9 = 
+                        
+                     `   <iframe name="demo" height="300" src="../Accessibility_quiz/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+        const HC10 = 
+                        
+                      `  <iframe name="demo" height="300" src="../Catphoto app/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+         const HC11 = 
+                        
+                       ` <iframe name="demo" height="300" src="../Catphoto app/index.html" width="400" style="border: 0px;"></iframe>
+                        `      
+        const HC12 = 
+                        
+                       ` <iframe name="demo" height="300" src="Catphoto app pro 1.html" width="400" style="border: 0px;"></iframe>`
+                        
+        const HC13 = 
+                        
+                        
+                       ` <iframe name="demo" height="300" src="../AT-Services/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+                        const HC14 = 
+                        
+                        
+                       ` <iframe name="demo" height="300" src="../Ferris_wheel/index.html" width="100%" style="border: 0px;"></iframe>
+                        `      
+                        const HC15 = 
+                        
+                        
+                       ` <iframe name="demo" height="300" src="Catphoto app pro 1.html" width="400" style="border: 0px;"></iframe>
+                        `
+                        
+                        
+         //JAVASCRIPT IFRAMES               
+         const JS1 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../GNS_103&105_test/exam.html" width="100%" style="border: 0px;"></iframe>
+                        `
+        const JS2 = 
+                        `
+                        <p>Scroll down to view</p>
+                        <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_1/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+         const JS3 = 
+                        `
+                        
+                        <iframe name="demo" height="300" id="gd2" src="../Game_Dev_Project/Game_Dev_2/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+        const JS4 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_3/GD 3A/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+        const JS5 = 
+                        `
+                        
+                        <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_3/GD 3B/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+         const JS6 = 
+                      `  <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_3/GD 3C/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+        const JS7 = 
+                        
+                    `   <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_3/GD 3D/index.html" width="100%" style="border: 0px;"></iframe>
+                        `  
+        const JS8 = 
+                        
+                      `  <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_4/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+
+    const JS9 = 
+                        
+                     `   <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_5/index.html" width="100%" style="border: 0px;"></iframe>
+                        `    
+        const JS10 = 
+                        
+                      `  <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_6/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+        const JS11 = 
+                        
+                       ` <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_7/index.html" width="100%" style="border: 0px;"></iframe>
+                        `
+     
+        const JS12 = 
+                        
+                       ` <iframe name="demo" height="300" src="../Game_Dev_Project/Game_Dev_8/index.html" width="100%" style="border: 0px;"></iframe>`
+                        
+        const JS13 = ` <iframe name="demo" height="300" src="../Catphoto app pro 1.html" width="400" style="border: 0px;"></iframe>
+                        `
+                        
+        const JS14 = 
+                        
+                        
+                       ` <iframe name="demo" height="340" src="Ferris wheel pro 14.html" width="400" style="border: 0px;"></iframe>
+                        `      
+
+     const JS15 = 
+                        
+                        
+                       ` <iframe name="demo" height="300" src="Catphoto app pro 1.html" width="400" style="border: 0px;"></iframe>
+                        `                
+     
+     //  HTML FUNCTIONS 
+      function HCassignbtn1() {
+           HCparagraph1.innerHTML = HC1;
+       }
+       function HCreturnbtn1() {
+           HCparagraph1.textContent = "";
+       }
+       
+      function HCassignbtn2() {
+           HCparagraph2.innerHTML = HC2;
+       }
+       function HCreturnbtn2() {
+           HCparagraph2.textContent = "";
+       }
+       
+       
+       
+       function HCassignbtn3() {
+           HCparagraph3.innerHTML = HC3;
+       }
+       function HCreturnbtn3() {
+           HCparagraph3.textContent = "";
+       }
+       
+       
+       
+       function HCassignbtn4() {
+           HCparagraph4.innerHTML = HC4;
+       }
+       function HCreturnbtn4() {
+           HCparagraph4.textContent = "";
+       }
+
+
+      function HCassignbtn5() {
+           HCparagraph5.innerHTML = HC5;
+       }
+      function HCreturnbtn5() {
+           HCparagraph5.textContent = "";
+       }
+       
+       
+       
+      function HCassignbtn6() {
+        HCparagraph6.innerHTML = HC6;
+       }
+      function HCreturnbtn6() {
+        HCparagraph6.textContent = "";
+       }
+       
+       
+       
+       
+      function HCassignbtn7() {
+        HCparagraph7.innerHTML = HC7;
+       }
+      function HCreturnbtn7() {
+        HCparagraph7.textContent = "";
+       }
+
+      function HCassignbtn8() {
+        HCparagraph8.innerHTML = HC8;
+       }
+      function HCreturnbtn8() {
+        HCparagraph8.textContent = "";
+       }
+       
+       
+       
+      function HCassignbtn9() {
+        HCparagraph9.innerHTML = HC9;
+       }
+      function HCreturnbtn9() {
+        HCparagraph9.textContent = "";
+       }
+       
+       
+
+       
+       function HCassignbtn10() {
+        HCparagraph10.innerHTML = HC10;
+       }
+       function HCreturnbtn10() {
+        HCparagraph10.textContent = "";
+       }
+       
+       
+       
+       function HCassignbtn11() {
+        HCparagraph11.innerHTML = HC11;
+       }
+       function HCreturnbtn11() {
+        HCparagraph11.textContent = "";
+       }
+       
+       
+       
+       
+       function HCassignbtn12() {
+        HCparagraph12.innerHTML = HC12;
+       }
+       function HCreturnbtn12() {
+        HCparagraph12.textContent = "";
+       }
+       
+       
+       
+       
+       function HCassignbtn13() {
+        HCparagraph13.innerHTML = HC13;
+       }
+       function HCreturnbtn13() {
+        HCparagraph13.textContent = "";
+       }
+      
+
+
+      function HCassignbtn14() {
+        HCparagraph14.innerHTML = HC14;
+       }
+       function HCreturnbtn14() {
+        HCparagraph14.textContent = "";
+       }
+       
+       
+       
+       function HCassignbtn15() {
+           HCparagraph15.innerHTML = HC15;
+       }
+       function HCreturnbtn15() {
+           HCparagraph15.textContent = "";
+       }
+
+     // JAVASCRIPT FUNCTIONS
+     
+     
+     function JSassignbtn1() {
+           JSparagraph1.innerHTML = JS1;
+       }
+       
+       function JSreturnbtn1() {
+        JSparagraph1.textContent = "";
+       }
+       
+       
+       
+       function JSassignbtn2() {
+        JSparagraph2.innerHTML = JS2;
+       }
+       function JSreturnbtn2() {
+        JSparagraph2.textContent = "";
+       }
+
+
+
+
+       function JSassignbtn3() {
+        JSparagraph3.innerHTML = JS3;
+       }
+       function JSreturnbtn3() {
+        JSparagraph3.textContent = "";
+       }
+       
+       
+       
+       function JSassignbtn4() {
+        JSparagraph4.innerHTML = JS4;
+       }
+       function JSreturnbtn4() {
+           JSparagraph4.textContent = "";
+       }
+      
+       function JSassignbtn5() {
+        JSparagraph5.innerHTML = JS5;
+       }
+       function JSreturnbtn5() {
+        JSparagraph5.textContent = "";
+       }
+       
+       
+       
+       function JSassignbtn6() {
+        JSparagraph6.innerHTML = JS6;
+       }
+       function JSreturnbtn6() {
+        JSparagraph6.textContent = "";
+       }
+       
+       
+       
+       
+       function JSassignbtn7() {
+        JSparagraph7.innerHTML = JS7;
+       }
+       function JSreturnbtn7() {
+        JSparagraph7.textContent = "";
+       }
+       
+       
+       
+       function JSassignbtn8() {
+        JSparagraph8.innerHTML = JS8;
+       }
+       function JSreturnbtn8() {
+        JSparagraph8.textContent = "";
+       }
+       
+       
+       
+       
+       function JSassignbtn9() {
+        JSparagraph9.innerHTML = JS9;
+       }
+       function JSreturnbtn9() {
+        JSparagraph9.textContent = "";
+       }
+       
+
+
+       function JSassignbtn10() {
+        JSparagraph10.innerHTML = JS10;
+       }
+       function JSreturnbtn10() {
+        JSparagraph10.textContent = "";
+       }
+       
+       
+       function JSassignbtn11() {
+        JSparagraph11.innerHTML = JS11;
+       }
+       function JSreturnbtn11() {
+        JSparagraph11.textContent = "";
+       }
+       
+       
+       
+       
+       function JSassignbtn12() {
+        JSparagraph12.innerHTML = JS12;
+       }
+       function JSreturnbtn12() {
+        JSparagraph12.textContent = "";
+       }
+       
+       
+       
+        function JSassignbtn13() {
+        JSparagraph13.innerHTML = JS13;
+       }
+       function JSreturnbtn13() {
+        JSparagraph13.textContent = "";
+       }
+      
+      
+      
+      
+      function JSassignbtn14() {
+        JSparagraph14.innerHTML = JS14;
+       }
+       function JSreturnbtn14() {
+        JSparagraph14.textContent = "";
+       }
+       
+       
+       
+       
+      function JSassignbtn15() {
+        JSparagraph15.innerHTML = JS15;
+       }
+      function JSreturnbtn15() {
+        JSparagraph15.textContent = "";
+       }
+       
+       
